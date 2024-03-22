a05db4140fc4695dc0efceef5bd8c09f83a80782
felicitaspineirob <162723113+felicitaspineirob@users.noreply.github.com>
2024-03-22 12:57:00 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,12 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name=f"{first_name.lower()} {last_name.lower()}"
+    print(full_name)
+    full_name=f"{first_name.title()} {last_name.title()}"
+    print(full_name)
+    full_name=f"{first_name.upper()} {last_name.upper()}"
+    print(full_name)
+    full_name=f"{"  "} {first_name.lower()} {last_name.lower()}"
+    print(full_name)
+    
